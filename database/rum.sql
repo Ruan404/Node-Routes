9b11507e2c825c03c261e79e91afd78c5318a089
ruan404 ruan404 <notezero231@gmail.com>
2024-10-31 14:39:14 +0100

    update UI

--- database/rum.sql
+++ database/rum.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  Dim 16 avr. 2023 à 15:34
--- Version du serveur :  5.7.26
--- Version de PHP :  7.2.18
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 31, 2024 at 01:37 PM
+-- Server version: 8.2.0
+-- PHP Version: 8.2.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,43 +18,43 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `rum`
+-- Database: `rum`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `city`
+-- Table structure for table `city`
 --
 
 DROP TABLE IF EXISTS `city`;
 CREATE TABLE IF NOT EXISTS `city` (
-  `id` int(13) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
   `village` varchar(8) NOT NULL,
   `name` varchar(20) NOT NULL,
   `image` varchar(120) NOT NULL,
-  `price` varchar(20) NOT NULL DEFAULT 'Indisponibles',
-  `contact` varchar(15) NOT NULL DEFAULT 'indisponible',
+  `price` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL DEFAULT '0-0',
+  `contact` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL DEFAULT '+000.000.000.0000',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
 
 --
--- Déchargement des données de la table `city`
+-- Dumping data for table `city`
 --
 
 INSERT INTO `city` (`id`, `village`, `name`, `image`, `price`, `contact`) VALUES
-(1, 'nuit', 'city_1', 'images/other-img/AnyConv.com__city.webp', 'Indisponibles', 'Indisponible'),
-(2, 'nuit', 'city_2', 'images/other-img/AnyConv.com__city1.webp', 'Indisponibles', 'Indisponible'),
-(3, 'nuit', 'city_3', 'images/other-img/AnyConv.com__city2.webp', 'Indisponibles', 'Indisponible'),
-(4, 'nuit', 'city_4', 'images/other-img/AnyConv.com__city.webp', '30,000 à 80,000', 'Indisponible'),
-(5, 'nuit', 'city_5', 'images/other-img/AnyConv.com__city1.webp', '30,000', 'Indisponible'),
-(6, 'nuit', 'city_6', 'images/other-img/AnyConv.com__city2.webp', '30,000', 'Indisponible'),
-(7, 'jour', 'city_7', 'images/other-img/AnyConv.com__city.webp', '40,000', 'Indisponible'),
-(8, 'jour', 'city_8', 'images/other-img/AnyConv.com__city1.webp', '25,000', 'Indisponible'),
-(9, 'jour', 'city_9', 'images/other-img/AnyConv.com__city2.webp', '40,000 à 50,000', 'Indisponible'),
-(10, 'nuit', 'city_10', 'images/other-img/AnyConv.com__city.webp', '50,000', 'Indisponible'),
-(11, 'jour', 'city_11', 'images/other-img/AnyConv.com__city1.webp', '40,000', 'Indisponible'),
-(12, 'jour', 'city_12', 'images/other-img/AnyConv.com__city2.webp', 'Indisponibles', 'Indisponible');
+(1, 'nuit', 'city1', 'images/other-img/AnyConv.com__city.webp', '0-0', '+000.000.000.0000'),
+(2, 'nuit', 'city2', 'images/other-img/AnyConv.com__city1.webp', '0-0', '+000.000.000.0000'),
+(3, 'nuit', 'city3', 'images/other-img/AnyConv.com__city2.webp', '0-0', '+000.000.000.0000'),
+(4, 'nuit', 'city4', 'images/other-img/AnyConv.com__city.webp', '30,000 - 80,000', '+000.000.000.0000'),
+(5, 'nuit', 'city5', 'images/other-img/AnyConv.com__city1.webp', '30,000', '+000.000.000.0000'),
+(6, 'nuit', 'city6', 'images/other-img/AnyConv.com__city2.webp', '30,000', '+000.000.000.0000'),
+(7, 'jour', 'city7', 'images/other-img/AnyConv.com__city.webp', '40,000', '+000.000.000.0000'),
+(8, 'jour', 'city8', 'images/other-img/AnyConv.com__city1.webp', '25,000', '+000.000.000.0000'),
+(9, 'jour', 'city9', 'images/other-img/AnyConv.com__city2.webp', '40,000 - 50,000', '+000.000.000.0000'),
+(10, 'nuit', 'city10', 'images/other-img/AnyConv.com__city.webp', '50,000', '+000.000.000.0000'),
+(11, 'jour', 'city11', 'images/other-img/AnyConv.com__city1.webp', '40,000', '+000.000.000.0000'),
+(12, 'jour', 'city12', 'images/other-img/AnyConv.com__city2.webp', '0-0', '+000.000.000.0000');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
